7a4e1a37cd9d3cab087894ba6019aacd9a98801e
Harshit <cr273916@gmail.com>
2026-01-29 15:19:01 +0530

    index

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Express 🚀");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
